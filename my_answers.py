3681e170b87573fa47a7717fcaf664066352b926
Laimonas Simutis <lsimutis@mediafly.com>
2017-07-19 23:08:05 -0500

    answers, etc

--- my_answers.py
+++ my_answers.py
@@ -3,15 +3,17 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import SimpleRNN
 import keras
 
-
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+
+    for i in range(len(series)-window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,12 +25,25 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5,input_shape=(window_size,1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+
+    #unwanted = ['"', '$', '%', '&', "'", '(', ')', '*', '-', '/', '?', '\xa0', '¢', '¨', '©', 'ã', '@', 'à', 'â', 'è', 'é']
+    
+    uniques = sorted(list(set(text)))
+    for u in uniques:
+        if u in punctuation:
+            continue
+        elif u.isalpha():
+            continue
+        text = text.replace(u, " ")
 
     return text
 
@@ -38,9 +53,20 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for a in range(0,len(text),step_size):
+        if (a+window_size < len(text)):
+            inputs.append(text[a:a+window_size])
+            outputs.append(text[a+window_size])
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    
+    model = Sequential()
+    model.add(LSTM(200,input_shape=(window_size,num_chars)))
+    #model.add(Dense(num_chars))
+    model.add(Dense(num_chars, activation='softmax'))
+
+    return model
